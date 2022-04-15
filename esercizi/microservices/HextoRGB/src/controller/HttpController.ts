5d594a14e04c7efbe8189709919785d644e88ca6
Fabros <markjasonfabrosgmail.com>
2022-04-15 19:34:38 +0200

    HexToRGB prova

---
+++ esercizi/microservices/HextoRGB/src/controller/HttpController.ts
@@ -0,0 +1,16 @@
+import {convert} from '../service/Service';
+import {Express} from 'express';
+import { TtfRgb, TtfHex } from "../../../commons/src/model/Color";
+
+class HttpController {
+    constructor(server: Express) {
+        server.get('/', (req, res) => {
+            const color = JSON.parse(req.query.color) as TtfHex;
+            const convertedColor: TtfRgb = convert(color);
+
+            res.send(convertedColor);
+        });
+    }
+}
+
+export default HttpController;
